e500bcde5cd151fda27e37dd740cf22fb0bac3a6
YJ-Arch <yj1516268@outlook.com>
2023-02-08 16:49:33 +0800

    添加构建/上传脚本

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+: << !
+Name: build.sh
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-02-08 16:46:28
+
+Description: 构建Python包
+
+Attentions:
+-
+
+Depends:
+-
+!
+
+python3 -m pip install --upgrade build
+python3 -m build
---
+++ upload.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+: << !
+Name: upload.sh
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-02-08 16:47:24
+
+Description: 上传构建好的Python包
+
+Attentions:
+-
+
+Depends:
+-
+!
+
+python3 -m pip install --upgrade twine
+python3 -m twine upload --repository pypi dist/*
