731ba65cfb695f8c9c70dd146fd408a56f438e7e
YJ-Arch <yj1516268@outlook.com>
2023-02-09 16:57:37 +0800

    脚本整合

--- package-robot.sh
+++ package-robot.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 : << !
-Name: 2.uploader.sh
+Name: package-robot.sh
 Author: YJ
 Email: yj1516268@outlook.com
-Created Time: 2023-02-08 16:47:24
+Created Time: 2023-02-08 16:46:28
 
-Description: 上传构建好的Python包
+Description: 构建并上传Python包，清除构建生成文件
 
 Attentions:
 -
@@ -15,6 +15,7 @@ Depends:
 -
 !
 
+# 进入虚拟环境
 readonly virtualenvwrapper="/usr/bin/virtualenvwrapper.sh"
 readonly virtualenvwrapper_lazy="/usr/bin/virtualenvwrapper_lazy.sh"
 
@@ -25,8 +26,14 @@ else
   # shellcheck source=/dev/null
   source "$virtualenvwrapper"
 fi
-
 workon build-upload
 
-python3 -m pip install --upgrade twine
+# 安装依赖包
+python3 -m pip install --upgrade build twine
+
+# 构建 & 上传
+python3 -m build
 python3 -m twine upload --repository pypi dist/*
+
+# 清除构建生成文件
+rm -rf dist src/logwrapper.egg-info
