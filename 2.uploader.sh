bca970c1d69123346afb113a732af30cbbd62232
YJ-Arch <yj1516268@outlook.com>
2023-02-09 16:52:29 +0800

    构建脚本增加虚拟环境

--- 2.uploader.sh
+++ 2.uploader.sh
@@ -15,5 +15,18 @@ Depends:
 -
 !
 
+readonly virtualenvwrapper="/usr/bin/virtualenvwrapper.sh"
+readonly virtualenvwrapper_lazy="/usr/bin/virtualenvwrapper_lazy.sh"
+
+if [ -s "$virtualenvwrapper_lazy" ]; then
+  # shellcheck source=/dev/null
+  source "$virtualenvwrapper_lazy"
+else
+  # shellcheck source=/dev/null
+  source "$virtualenvwrapper"
+fi
+
+workon build-upload
+
 python3 -m pip install --upgrade twine
 python3 -m twine upload --repository pypi dist/*
